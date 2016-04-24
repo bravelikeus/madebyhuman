061b9a5042921e50c9e4699208f3c4b3f04b33c9
ryanincolor <ryan@bravelikeus.com>
2016-04-24 23:17:05 +0200

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
